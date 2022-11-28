e1f5fa06c328f7c43088fd688e971e2393804158
= <=>
2022-11-28 18:01:11 +0700

    floating_action_button

--- basic_widget/lib/main.dart
+++ basic_widget/lib/main.dart
@@ -5,23 +5,18 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        margin: const EdgeInsets.only(top: 30),
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            AppBar(title: const Text('Contoh Cupertino')),
-            CupertinoButton(
-              child: const Text("Contoh button"),
-              onPressed: () {},
-            ),
-            const CupertinoActivityIndicator(),
-          ],
-        ),
+        home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() {
+          //Add your pressed code here
+        }),
+        child: const Icon(Icons.thumb_up),
+        backgroundColor: Colors.pink,
       ),
-    );
+    ));
   }
 }
