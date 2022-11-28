b40190f5237081f4c789bbfaf301c0ad8e11416b
= <=>
2022-11-28 22:17:49 +0700

    listview_widget

--- basic_widget/lib/main.dart
+++ basic_widget/lib/main.dart
@@ -9,28 +9,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(title: const Text("Contoh Stack Widget")),
+      appBar: AppBar(title: const Text("Contoh ListView Widget")),
       // ignore: avoid_unnecessary_containers
-      body: Stack(
+      body: ListView(
         children: <Widget>[
           Container(
             color: Colors.green,
-            alignment: Alignment.bottomCenter,
-            child: const Text("Satu",
+            alignment: Alignment.topLeft,
+            child: const Text(
+                "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                 style: TextStyle(fontSize: 30, color: Colors.white)),
           ),
           Container(
             color: Colors.red,
-            alignment: Alignment.bottomCenter,
-            child: const Text("Dua",
+            alignment: Alignment.topLeft,
+            child: const Text(
+                "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                 style: TextStyle(fontSize: 30, color: Colors.white)),
             height: 400.0,
             width: 300.0,
           ),
           Container(
             color: Colors.deepPurple,
-            alignment: Alignment.bottomCenter,
-            child: const Text("Tiga",
+            alignment: Alignment.topLeft,
+            child: const Text(
+                "ListView widget digunanakan untuk menampilkan data dalam bentuk list dan jika datanya melebihi dari render box maka halaman tersebut dapat di scroll.",
                 style: TextStyle(fontSize: 30, color: Colors.white)),
             height: 200.0,
             width: 200.0,
