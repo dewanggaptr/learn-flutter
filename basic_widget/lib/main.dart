648de03a31fa5fe53f1ddd8b4906af977c339214
= <=>
2022-11-28 21:20:36 +0700

    decoration_property

--- basic_widget/lib/main.dart
+++ basic_widget/lib/main.dart
@@ -9,10 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: const Text("Contoh Transform")),
+          appBar: AppBar(title: const Text("Contoh Configuration")),
           // ignore: avoid_unnecessary_containers
           body: Container(
             decoration: BoxDecoration(
+              color: const Color(0xff7c94b6),
               image: const DecorationImage(
                 image: NetworkImage(
                     'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'),
@@ -22,11 +23,11 @@ class MyApp extends StatelessWidget {
                 color: Colors.black,
                 width: 8,
               ),
+              borderRadius: BorderRadius.circular(12),
             ),
             height: 200,
             width: 300,
-            margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
-            transform: Matrix4.rotationZ(-0.1),
+            margin: const EdgeInsets.all(20),
           )),
     );
   }
