977f8c97204c1a4cd531ce6ccdda982cd9a75058
= <=>
2022-11-28 21:07:55 +0700

    height_and_weight_property

--- basic_widget/lib/main.dart
+++ basic_widget/lib/main.dart
@@ -9,14 +9,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: const Text("Contoh Color")),
+          appBar: AppBar(title: const Text("Contoh Height and Weight")),
           // ignore: avoid_unnecessary_containers
           body: Container(
-              margin: const EdgeInsets.all(100),
-              height: 200,
+              margin: const EdgeInsets.all(133),
+              height: 150,
               width: 200,
               alignment: Alignment.center,
-              color: Colors.amber[900],
+              color: Colors.blueGrey,
               child: const Text(
                 'Semangat Belajar',
                 style: TextStyle(fontSize: 20, color: Colors.white),
