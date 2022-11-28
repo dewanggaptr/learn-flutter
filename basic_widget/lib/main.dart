d2facf7286cb59d35a568650257964085feec41a
= <=>
2022-11-28 21:01:23 +0700

    alignment_property

--- basic_widget/lib/main.dart
+++ basic_widget/lib/main.dart
@@ -9,19 +9,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: const Text("Contoh Button")),
+          appBar: AppBar(title: const Text("Contoh Alignment")),
           // ignore: avoid_unnecessary_containers
           body: Container(
-            margin: const EdgeInsets.all(10.0),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                textStyle: const TextStyle(color: Colors.white),
-              ),
-              onPressed: () {},
-              child: const Text("Button", style: TextStyle(fontSize: 20)),
-            ),
-          )),
+              alignment: Alignment.bottomCenter,
+              child: const Text(
+                'Semangat Belajar',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ))),
     );
   }
 }
